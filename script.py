98ba7c88082ff06ab51efa7f78ea3bca7ed21936
TimTieng <timothy.tieng@gmail.com>
2021-03-24 15:53:28 -0400

    finished project; added several methods to patient class

--- script.py
+++ script.py
@@ -18,6 +18,35 @@ class Patient:
     def updateAge(self, newAge):
         self.age = newAge
         print(f"{self.name}'s updated age is {self.age} years old")
+        # update insurance cost to reflect new age- call the method
+        self.estimatedInsuranceCost()
+    # UpdateChildrenAmount()
+    def updateChildrenAmount(self, newChildrenAmount):
+        self.numOfChildren = newChildrenAmount
+        if self.numOfChildren == 1:
+            print(f"{self.name} has {self.numOfChildren} child")
+        else:
+            print(f"{self.name} has {self.numOfChildren} children")
+        self.estimatedInsuranceCost()
+    # updateBMI
+    def updateBMI(self,newBMI):
+        self.bmi = newBMI
+        self.estimatedInsuranceCost()
+    # updateSmokerStatus()
+    def updateSmokerStatus(self, newSmokerVal):
+        self.smoker = newSmokerVal
+        self.estimatedInsuranceCost()
+    # Create a dictionary method per patient
+    def createPatientDatabase(self):
+        patientInformation = {}
+        patientInformation['name'] = self.name
+        patientInformation['age'] = self.age
+        patientInformation['sex'] = self.sex
+        patientInformation['bmi'] = self.bmi
+        patientInformation['Number Of Children'] = self.numOfChildren
+        patientInformation['Smoker'] = self.smoker
+        return patientInformation
+
 
 # Create an object of Patient to test
 patient1 = Patient("Tim Tieng",32, 1, 20.0, 1, 0)
@@ -28,3 +57,10 @@ patient1.estimatedInsuranceCost()
 
 # Test to see if UpdatedAge() works
 patient1.updateAge(33)
+patient1.estimatedInsuranceCost()
+
+# Test updatedChildrenAmmount
+patient1.updateChildrenAmount(2)
+
+# Test Dictionary Creation method
+print(patient1.createPatientDatabase())
